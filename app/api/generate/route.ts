3e7e7d24fc5fb691c692ae062bc1cb6f2244b474
super-website <kh.muhavia1@gmail.com>
2026-09-08 16:31:36 +0500

    Update design

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 })
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
